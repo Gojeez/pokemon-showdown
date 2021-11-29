2b5cb85e31a35def281542168fe1e4044c64277c
Gojee <67485015+Gojeez@users.noreply.github.com>
2021-11-28 19:32:03 -0500

    Update pokedex.ts

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -77,7 +77,7 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 	charmander: {
 		num: 4,
 		name: "Radeon",
-		types: ["Fire"],
+		types: ["Fire", "Steel"],
 		genderRatio: {M: 0.875, F: 0.125},
 		baseStats: {hp: 39, atk: 52, def: 43, spa: 60, spd: 50, spe: 65},
 		abilities: {0: "Blaze", H: "Solar Power"},
